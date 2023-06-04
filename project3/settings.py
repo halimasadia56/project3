93c5c420ca10f368fac718c5616b8b34c3a992f1
halimasadia56 <halimasadia56@gmail.com>
2023-06-04 18:04:56 +0530

    first commit

--- project3/settings.py
+++ project3/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR=BASE_DIR/'templates'
+import os
+TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
 
 
 # Quick-start development settings - unsuitable for production
